f93ddf61ff1114ae264108ed640d52e315512a59
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2022-06-09 11:10:08 -0700

    Added Template

---
+++ src/test/java/com/directrix/Directrix/DirectrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.directrix.Directrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
